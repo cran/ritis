e100638c2448fdbcc253c8f303c5eaba40b462f5
Scott Chamberlain <myrmecocystus@gmail.com>
2012-11-06 00:00:00 +0000

    version 0.0.2

--- inst/tests/test-getdescription.R
+++ inst/tests/test-getdescription.R
@@ -1,10 +1,6 @@
 # tests for getdescription fxn in ritis
 context("getdescription")
 
-test_that("getdescription returns the correct value", {
-	expect_that(getdescription(), equals("This is the ITIS Web Service, providing access to the data behind www.itis.gov. The database contains 607,608 scientific names (463,086 of them valid/accepted) and 116,477 common names."))
-})
-
-test_that("getdescription returns the correct number of characters", {
-	expect_that(nchar(getdescription()), equals(185))
+test_that("getdescription returns the correct class", {
+	expect_that(getdescription(), is_a("character"))
 })
